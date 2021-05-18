ca505abc80b44b51dceaa83d7df41690ac646731
Daniel Grew <Dgrew0287@conestogac.on.ca>
2021-05-18 17:29:02 -0400

    Added tests for the findServer() function

--- test/findServerTests.js
+++ test/findServerTests.js
@@ -127,5 +127,103 @@ describe('Test checkServer()', () => {
               }).catch(done)
           })
     })
-    
+
+    // TEST 2
+    // Function: checkServer()
+    // Description: This test will test the happy path for a server that does not exist
+    // Expected return: null
+    describe('Test 2 - checkServer() with Offline server', () => {
+        const test_online =     {
+            "url": "http://gooasdawgle.com",
+            "priority": 4
+          }
+
+          it('Should check if google.com is online and return null', (done) => {
+              checkServer(test_online).then(res => {
+                expect(res).to.be.eq(null);
+                done();
+              }).catch(done)
+          })
+    })
 })
+
+// test the findServer function
+describe('Test findServer()', () => {
+    // TEST 3
+    // Function: findServer()
+    // Description: This test will test the happy path for a list of servers 
+    // Expected return: { "url": "http://google.com","priority": 4 }
+    describe('Test 3 - Test the findServer for list containing at least one online server', () => {
+        it('Should return { "url": "http://google.com","priority": 4 }', (done) => {
+            findServer(test).then( res => {
+                expect(res).to.be.an('object').eq(test[3])
+                done();
+            }).catch(done)
+        })
+    })
+    
+    // TEST 4
+    // Function: findServer()
+    // Description: This test will test the happy path for a list of servers that all do not exist
+    // Expected return: Error
+    describe('Test 4 - Test the findServer for list containing no online servers', () => {
+        it('Should throw an error', (done) => {
+            findServer(test2).then(done()).catch(err => {
+                expect(err).to.be.an('error').eq(new Error("None of the servers are online"));
+                done();
+            })
+        })
+    })
+    // TEST 5
+    // Function: findServer()
+    // Description: This test will test the happy path for a list of servers two of which exist
+    // Expected return: { "url": "http://boldtech.co","priority": 2 }
+    describe('Test 5 - Test the findServer for list containing at least one online server', () => {
+        it('Should return { "url": "http://boldtech.co","priority": 2 }', (done) => {
+            findServer(test3).then( res => {
+                expect(res).to.be.an('object').eq(test3[1])
+                done();
+            }).catch(done)
+        })
+    })
+
+    // TEST 6
+    // Function: findServer()
+    // Description: This test will test the happy path for a list of servers two of which exist
+    // Expected return: { "url": "http://boldtech.co","priority": 2 }
+    describe('Test 6 - Test the findServer for list containing two online servers with the same priority', () => {
+        it('Should return { "url": "http://google.com","priority": 4 }', (done) => {
+            findServer(test4).then( res => {
+                expect(res).to.be.an('object').eq(test4[3])
+                done();
+            }).catch(done)
+        })
+    })
+
+    // TEST 7
+    // Function: findServer()
+    // Description: This test will test the function when an empty list is passed
+    // Expected return: Error
+    describe('Test 7 - Test the findServer for list containing no online servers', () => {
+        it('Should throw an error', (done) => {
+            findServer(test5).then(done()).catch(err => {
+                expect(err).to.be.an('error').eq(new Error("None of the servers are online"));
+                done();
+            })
+        })
+    })
+
+    // TEST 8
+    // Function: findServer()
+    // Description: This test will test the function when an object containing an incorrect element is passed
+    // Expected return: Error
+    describe('Test 8 - Test the findServer using a function containing an incorrect element', () => {
+        it('Should throw an error', (done) => {
+            findServer(test5).then(done()).catch(err => {
+                expect(err).to.be.an('error').eq(new Error("The Server Details object is missing required details about the server"));
+                done();
+            })
+        })
+    })
+})
+
